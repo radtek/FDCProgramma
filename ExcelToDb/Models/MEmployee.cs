28480c1b3713ab7dff09620a405019dc5a88efda
AF <1197827679@qq.com>
2018-11-08 17:44:30 +0800

    增加员工信息对象类

---
+++ ExcelToDb/Models/MEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    /// <summary>
+    /// 员工信息类
+    /// </summary>
+    public class MEmployee
+    {
+
+    }
+}
