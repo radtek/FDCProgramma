7e98d30ceceff5aefc74bf5208072f267c0241ae
AF <1197827679@qq.com>
2018-11-16 17:22:14 +0800

    分配逻辑更新

--- ExcelToDb/BLL/Distribute/BAllotHandle.cs
+++ ExcelToDb/BLL/Distribute/BAllotHandle.cs
@@ -14,7 +14,7 @@ namespace BLL.Distribute
     public  class BAllotHandle
     {
         DAllotHandle DAllotHandle;
-        private AdminMsg LocalUser = "";
+        private AdminMsg LocalUser;
         public BAllotHandle(AdminMsg LoginMsg)
         {
             
@@ -44,12 +44,59 @@ namespace BLL.Distribute
             //当数据剩余量小于需要的数据量时
             /*
              * 处理逻辑？
+             * 1.先判断数据剩余量能不能平分到每个人相同的量
+             * 2.剩余的不作处理，只处理满足平分条件的
+             * 3. 
              */
             if (SourceRowsCount < TableCount)
             {
+                /*
+                 * Linq使用博客地址：https://blog.csdn.net/zlbcdn/article/details/79192485
+                 */
+                //单人任务量
+                int SingleTask;
+                /*
+                 * 正好整除，不用再剔除剩余的
+                 */
+                if (SourceRowsCount % EmployeeCount == 0)
+                {
+                    SingleTask = SourceRowsCount / EmployeeCount;
+                    DataRowCollection TheRows = SourceTable.Rows;
+                    for (int i = 0; i < EmployeeCount; i++)
+                    {
+                        //每次只取单人任务量的行进行操作
+                        //var TheRows = (from DataRowCollection cc in SourceTable.Rows select cc).Skip(i*SingleTask).Take(SingleTask);
+                        DataRow[] LinqRows = TheRows.OfType<DataRow>().Skip(i * SingleTask).Take(SingleTask).ToArray();
+                        foreach (DataRow item in LinqRows)
+                        {
+                            string Name = (string)item["Name"];
+                        }
+
+
+
+
+
+
+                    }
+                    List<DataRow> TaskRows = new List<DataRow>(SingleTask);
+                }
+                /*
+                 * 剔除多余的数据再进行拆分的2中情况
+                 * 1.数据量太少不够一次分配的
+                 * 2.数据量够分配数次还有剩余
+                 */
+
+
+
+
+
 
             }
-            //当数据剩余量等于需要的数据量时，正常处理
+            /*
+             * 2种情况
+             * 1.当数据剩余量等于需要的数据量时
+             * 2.当剩余数据量远远大于需要的数据量时
+             */
             else
             {
 
