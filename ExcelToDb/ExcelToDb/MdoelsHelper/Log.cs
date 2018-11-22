590a83c1b59b3f64de02211473587a42561beeef
AF <1197827679@qq.com>
2018-11-22 17:44:32 +0800

    文件大小判断

--- ExcelToDb/ExcelToDb/MdoelsHelper/Log.cs
+++ ExcelToDb/ExcelToDb/MdoelsHelper/Log.cs
@@ -70,5 +70,46 @@ namespace ExcelToDb.MdoelsHelper
                 directoryInfo.Create();
             }
         }
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        /// <param name="sFullName"></param>
+        /// <returns></returns>
+        public static long GetFileSize(string sFullName)
+        {
+            long lSize = 0;
+            if (File.Exists(sFullName))
+                lSize = new FileInfo(sFullName).Length;
+            return lSize;
+        }
+        /// <summary>
+        /// 计算文件大小函数(保留两位小数),Size为字节大小
+        /// </summary>
+        /// <param name="Size"></param>
+        /// <returns></returns>
+        public static string CountSize(long Size)
+        {
+            string m_strSize = "";
+            long FactSize = 0;
+            FactSize = Size;
+            if (FactSize < 1024.00)
+                m_strSize = FactSize.ToString("F2") + "Byte";
+            else if (FactSize >= 1024.00 && FactSize < 1048576)
+                m_strSize = (FactSize / 1024.00).ToString("F2") + "K";
+            else if (FactSize >= 1048576 && FactSize < 1073741824)
+                m_strSize = (FactSize / 1024.00 / 1024.00).ToString("F2") + "M";
+            else if (FactSize >= 1073741824)
+                m_strSize = (FactSize / 1024.00 / 1024.00 / 1024.00).ToString("F2") + "G";
+            return m_strSize;
+        }
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        /// <param name="FilePath"></param>
+        /// <returns>/1M/2G</returns>
+        public string GetFileLength(string FilePath)
+        {
+            return CountSize(GetFileSize(FilePath));
+        }
     }
 }
